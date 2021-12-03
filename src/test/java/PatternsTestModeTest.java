170b867efa81ae3eb0663c2e28bea93459748b6c
hYxA <zybu@live.ru>
2021-12-03 12:14:48 +0300

    configuration

---
+++ src/test/java/PatternsTestModeTest.java
@@ -0,0 +1,8 @@
+/**
+ * Для активации этого тестового режима при запуске SUT
+ * нужно указать флаг -P:profile=test,
+ * т.е.: java -jar app-ibank.jar -P:profile=test.
+ **/
+
+public class PatternsTestModeTest {
+}
