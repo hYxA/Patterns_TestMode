47965ab275e5d16ec0a68e1e4779196ed91fcb57
hYxA <zybu@live.ru>
2021-12-03 12:29:37 +0300

    light correct

--- src/test/java/PatternsTestModeTest.java
+++ src/test/java/PatternsTestModeTest.java
@@ -1,7 +1,7 @@
 /**
  * Для активации этого тестового режима при запуске SUT
  * нужно указать флаг -P:profile=test,
- * т.е.: java -jar app-ibank.jar -P:profile=test.
+ * т.е.: java -jar .\artifacts\app-ibank.jar -P:profile=test.
  **/
 
 public class PatternsTestModeTest {
