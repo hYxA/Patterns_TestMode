69fbd5897a45090791b9d6cdd8efa321328adb27
hYxA <zybu@live.ru>
2021-12-07 14:37:28 +0300

    UI Tests passed

--- src/test/java/ru/netology/testmode/AuthTestModeVia_UITest.java
+++ src/test/java/ru/netology/testmode/AuthTestModeVia_UITest.java
@@ -1,7 +1,5 @@
 package ru.netology.testmode;
 
-import com.codeborne.selenide.Condition;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -13,7 +11,6 @@ import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static io.restassured.RestAssured.given;
 import static ru.netology.data.DataGenerator.*;
 
 /**
@@ -26,95 +23,90 @@ public class AuthTestModeVia_UITest {
 
     @BeforeEach
     @Test
-    void setTest(){
+    void setTest() {
         open("http://localhost:9999");
     }
 
 
     @Test
     @DisplayName("Should successfully login with active registered user")
-    void shouldSuccessfulLoginIfRegisteredActiveUser() {
+    void shouldSuccessfulLoginIfRegisteredActiveUser_via_UI() {
         RegistrationDto registeredUser = genValidActiveUser();
 
-       $("[data-test-id=login] input").sendKeys(registeredUser.getLogin());
-       $("[data-test-id=password] input").sendKeys(registeredUser.getPassword());
-       $("[data-test-id=action-login] button__content").click();
-       $(byText("Личный кабинет")).shouldBe(visible, Duration.ofSeconds(5));
+        $("[data-test-id=login] input").sendKeys(registeredUser.getLogin());
+        $("[data-test-id=password] input").sendKeys(registeredUser.getPassword());
+        $("[data-test-id=action-login]").click();
+        $(byText("Личный кабинет")).shouldBe(visible, Duration.ofSeconds(5));
 
     }
-//
-//
-//    @Test
-//    @DisplayName("Should get error message if login with not registered user")
-//    void shouldGetErrorIfNotRegisteredUser() {
-//        RegistrationDto notRegisteredUser = genInvalidLogin();
-//
-//        given() // "дано"
-//                .spec(requestSpec)
-//                .body(new RegistrationDto(
-//                        notRegisteredUser.getLogin(),
-//                        notRegisteredUser.getPassword(),
-//                        "active"))
-//                .when()
-//                .post("/api/system/users")
-//                .then()
-//                .statusCode(200);
-//    }
-//
-//
-//    @Test
-//    @DisplayName("Should get error message if login with blocked registered user")
-//    void shouldGetErrorIfBlockedUser() {
-//        RegistrationDto blockedUser = genValidBlockedUser();
-//
-//        given() // "дано"
-//                .spec(requestSpec)
-//                .body(new RegistrationDto(
-//                        blockedUser.getLogin(),
-//                        blockedUser.getPassword(),
-//                        "blocked"))
-//                .when()
-//                .post("/api/system/users")
-//                .then()
-//                .statusCode(200);
-//    }
-//
-//    @Test
-//    @DisplayName("Should get error message if login with wrong login")
-//    void shouldGetErrorIfWrongLogin() {
-//        RegistrationDto registeredUser = genValidActiveUser();
-//        String wrongLogin = faker.name().username();
-//
-//        given() // "дано"
-//                .spec(requestSpec)
-//                .body(new RegistrationDto(
-//                        wrongLogin,
-//                        registeredUser.getPassword(),
-//                        "active"))
-//                .when()
-//                .post("/api/system/users")
-//                .then()
-//                .statusCode(200);
-//    }
-//
-//    @Test
-//    @DisplayName("Should get error message if login with wrong password")
-//    void shouldGetErrorIfWrongPassword() {
-//        RegistrationDto registeredUser = genValidActiveUser();
-//        String wrongPassword = faker.internet().password();
-//
-//        given() // "дано"
-//                .spec(requestSpec)
-//                .body(new RegistrationDto(
-//                        registeredUser.getLogin(),
-//                        wrongPassword,
-//                        "active"))
-//                .when()
-//                .post("/api/system/users")
-//                .then()
-//                .statusCode(200);
-//    }
+
+
+    @Test
+    @DisplayName("Should get error message if login with not registered user")
+    void shouldGetErrorIfNotRegisteredUser_via_UI() {
+        RegistrationDto notRegisteredUser = genInvalidLogin();
+
+        $("[data-test-id=login] input").sendKeys(notRegisteredUser.getLogin());
+        $("[data-test-id=password] input").sendKeys(notRegisteredUser.getPassword());
+        $("[data-test-id=action-login]").click();
+        $(byText("Неверно указан логин или пароль")).shouldBe(visible, Duration.ofSeconds(5));
+    }
+
+
+    @Test
+    @DisplayName("Should get error message if login with blocked registered user")
+    void shouldGetErrorIfBlockedUser_via_UI() {
+        RegistrationDto blockedUser = genValidBlockedUser();
+
+        $("[data-test-id=login] input").sendKeys(blockedUser.getLogin());
+        $("[data-test-id=password] input").sendKeys(blockedUser.getPassword());
+        $("[data-test-id=action-login]").click();
+        $(byText("Пользователь заблокирован")).shouldBe(visible, Duration.ofSeconds(5));
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with wrong login")
+    void shouldGetErrorIfWrongLogin_via_UI() {
+        RegistrationDto notRegisteredUser = genInvalidLogin();
+
+        $("[data-test-id=login] input").sendKeys(notRegisteredUser.getLogin());
+        $("[data-test-id=password] input").sendKeys(notRegisteredUser.getPassword());
+        $("[data-test-id=action-login]").click();
+        $(byText("Неверно указан логин или пароль")).shouldBe(visible, Duration.ofSeconds(5));
+    }
+
+    @Test
+    @DisplayName("Should get error message if login with wrong password")
+    void shouldGetErrorIfWrongPassword_via_UI() {
+        RegistrationDto registeredUser = genBadPassword();
+
+        $("[data-test-id=login] input").sendKeys(registeredUser.getLogin());
+        $("[data-test-id=password] input").sendKeys(registeredUser.getPassword());
+        $("[data-test-id=action-login]").click();
+        $(byText("Неверно указан логин или пароль")).shouldBe(visible, Duration.ofSeconds(5));
+    }
+
+    @Test
+    @DisplayName("Should get error message if login is empty")
+    void shouldGetErrorIfLoginIsEmpty_via_UI() {
+        RegistrationDto registeredUser = genValidActiveUser();
+
+        $("[data-test-id=password] input").sendKeys(registeredUser.getPassword());
+        $("[data-test-id=action-login]").click();
+        $(byText("Поле обязательно для заполнения")).shouldBe(visible, Duration.ofSeconds(5));
+    }
+
+    @Test
+    @DisplayName("Should get error message if password is empty")
+    void shouldGetErrorIfPasswordIsEmpty_via_UI() {
+        RegistrationDto registeredUser = genValidActiveUser();
+
+        $("[data-test-id=login] input").sendKeys(registeredUser.getLogin());
+        $("[data-test-id=action-login]").click();
+        $(byText("Поле обязательно для заполнения")).shouldBe(visible, Duration.ofSeconds(5));
+    }
 }
+
 
 
 
